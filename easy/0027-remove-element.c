14d651d0a27b00de7eca8a6352c72c39beff12d5
Siddhesh Girish Dhok <siddheshdhok10@gmail.com>
2025-11-11 11:05:53 +0530

    Add solution for 0027 Remove Element

---
+++ easy/0027-remove-element.c
@@ -0,0 +1,13 @@
+# Problem: 20. Valid Parentheses
+# Difficulty: Easy
+# URL: https://leetcode.com/problems/remove-element/
+
+int removeElement(int* nums, int numsSize, int val) {
+    int k = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[k++] = nums[i];
+        }
+    }
+    return k;
+}
